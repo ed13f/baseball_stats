599b790ab1bdc29e49cd851321c98f4e6fc88fc0
Enrico De Santis <enricodesantis@Enricos-MacBook-Pro.local>
2020-01-05 14:27:46 -0500

    Updates

---
+++ precache-manifest.45d573aff623df6ec793fe6da4ed5a8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74a83ef1f5a1a7a69ea936eb830d7c2",
+    "url": "/baseball_stats/index.html"
+  },
+  {
+    "revision": "4d4e9396c60c7af58142",
+    "url": "/baseball_stats/static/css/main.790ee2b7.chunk.css"
+  },
+  {
+    "revision": "b24d10ebf7bc4b38d425",
+    "url": "/baseball_stats/static/js/2.c1009298.chunk.js"
+  },
+  {
+    "revision": "4d4e9396c60c7af58142",
+    "url": "/baseball_stats/static/js/main.122bedc1.chunk.js"
+  },
+  {
+    "revision": "cf4e0a8fdb3edf52b907",
+    "url": "/baseball_stats/static/js/runtime-main.38ae4df6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/baseball_stats/precache-manifest.45d573aff623df6ec793fe6da4ed5a8b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/baseball_stats/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
