9b0e9b100284be3e1f14b1b77fd3b3d55e9b483c
Enrico De Santis <enricodesantis@Enricos-MacBook-Pro.local>
2020-01-21 19:52:50 -0500

    Updates

--- precache-manifest.1d6cc80a23f3babf340e0a95ba60e9cc.js
+++ precache-manifest.1d6cc80a23f3babf340e0a95ba60e9cc.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f7d5425087fdfb96c13c6ec8066f84c6",
+    "revision": "db98b2c1559b4ce5c3ebcead90ca3969",
     "url": "/baseball_stats/index.html"
   },
   {
-    "revision": "7a834cb826ef70b63111",
-    "url": "/baseball_stats/static/css/main.69983b8a.chunk.css"
+    "revision": "d0decb7ac7924e2bafb9",
+    "url": "/baseball_stats/static/css/main.17d48657.chunk.css"
   },
   {
     "revision": "52077ee712175685480b",
     "url": "/baseball_stats/static/js/2.5ec01d8c.chunk.js"
   },
   {
-    "revision": "7a834cb826ef70b63111",
-    "url": "/baseball_stats/static/js/main.5ef9df9b.chunk.js"
+    "revision": "d0decb7ac7924e2bafb9",
+    "url": "/baseball_stats/static/js/main.abe37389.chunk.js"
   },
   {
     "revision": "cf4e0a8fdb3edf52b907",
